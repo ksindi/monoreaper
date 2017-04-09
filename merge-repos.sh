9885ae28e5be0c248e607a6834ad8933dc7f4d8d
Kamil Sindi <ksindi@users.noreply.github.com>
2017-04-09 18:22:53 -0400

    Create merge-repos.sh

---
+++ merge-repos.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Merge multiple GitHub repos into a master-repo.
+#
+# Usage: bash merge-repos.sh master-dir owner0/repo0 owner1/repo1
+
+# check number of arguments at least 1
+if (( $# < 2 )); then
+  printf 'Error: At least one repo required.\n'
+  printf 'Usage: bash merge-repos.sh master-dir owner0/repo0 owner1/repo1'
+  printf 'Exiting...\n'
+  exit 1
+fi
+
+WORK_DIR=/tmp/github-mergers
+rm -rf $WORK_DIR
+WORK_SRC_DIR=$WORK_DIR/src && mkdir -p $WORK_SRC_DIR
+WORK_TGT_DIR=$WORK_DIR/tgt && mkdir -p $WORK_TGT_DIR
+MASTER_DIR=$1 && mkdir $MASTER_DIR
+
+# get github credentials
+read -p "Git Username: " GH_UNAME
+read -s -p "Git Password: " GH_PASSWD
+printf '\n'
+
+#######################################
+# Move repo contents into a subdirectory of the same name
+# Globals:
+#   WORK_SRC_DIR
+#   WORK_TGT_DIR
+# Arguments:
+#   Repository name
+# Returns:
+#   None
+#######################################
+prepare_repo () {
+  local repo_name=$1
+  local old_repo=$WORK_SRC_DIR/$repo_name
+  local new_repo=$WORK_TGT_DIR/$repo_name
+  mkdir -p $new_repo
+  mv $old_repo/.git/ $new_repo
+  mv $old_repo/.gitignore $new_repo
+  mv $old_repo/ $new_repo/$repo_name
+  pushd $new_repo
+  git add $repo_name
+  git commit -am "Preparing ${repo_name} repository for merging."
+  popd
+}
+
+# create master-repo directory
+pushd $MASTER_DIR
+git init
+touch README.md
+git add README.md
+git commit -am 'Inital Commit'
+# create clean branch which will be used to stage new repos
+git branch clean
+
+for repo in "${@:2}"
+do
+  git checkout clean
+  IFS='/' read owner repo_name <<<$repo
+  gh_url="https://${GH_UNAME}:${GH_PASSWD}@github.com/${owner}/${repo_name}.git"
+  git clone $gh_url $WORK_SRC_DIR/$repo_name
+  prepare_repo $repo_name
+  remote_name=remote_${repo_name}
+  git remote add $remote_name $WORK_TGT_DIR/$repo_name
+  git fetch --all
+  git branch $repo_name $remote_name/master
+  git checkout $repo_name
+  git checkout master
+  git merge --squash $repo_name master
+  git commit -am "Squashed ${repo_name}"
+  git remote remove $remote_name 
+done
+popd
+
+# clean up
+printf 'Cleaning up...\n'
+rm -rf WORK_DIR
